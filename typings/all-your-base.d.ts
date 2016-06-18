eabb6c75755695d690ade2e47c364be60d8824d6
Jeremy Foster <jeremy.foster@live.com>
2016-06-17 20:19:55 -0700

    remove typings folder from source control

--- typings/all-your-base.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'all-your-base' {
-    var ayb:AllYourBase;
-    export = ayb;
-}
-
-declare interface AllYourBase {
-    hexToDec(value:string): number,
-    decToHex(value:number): string
-}
--- typings/index.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference path="globals/socket.io-client/index.d.ts" />
-/// <reference path="modules/lodash/index.d.ts" />
